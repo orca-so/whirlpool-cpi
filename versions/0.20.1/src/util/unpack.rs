b2e5ac21f9b5352718d552353c454a5c5e435082
yugure <109891005+yugure-orca@users.noreply.github.com>
2024-01-12 09:02:58 +0900

    fix 0.20.1

--- versions/0.20.1/src/util/unpack.rs
+++ versions/0.20.1/src/util/unpack.rs
@@ -1,10 +1,8 @@
 use anchor_lang::prelude::*;
-use bincode;
-use default_boxed::DefaultBoxed;
 
 use crate::state::{TickArray, Tick, TICK_ARRAY_SIZE_USIZE, NUM_REWARDS};
 
-#[derive(Debug, PartialEq, Clone, Copy, DefaultBoxed)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub struct UnpackedTickArray {
   pub start_tick_index: i32,
   pub ticks: [UnpackedTick; TICK_ARRAY_SIZE_USIZE],
@@ -21,47 +19,21 @@ pub struct UnpackedTick {
     pub reward_growths_outside: [u128; NUM_REWARDS],
 }
 
-pub fn unpack_tick_array(loader: &AccountLoader<TickArray>) -> std::result::Result<Box<UnpackedTickArray>, Error> {
-  try_deserialize_tick_array(
-    loader.to_account_info().owner,
-    &mut loader.to_account_info().data.borrow().as_ref(),
-  )
-}
+pub fn unpack_tick_array(loader: &AccountLoader<TickArray>) -> std::result::Result<Box<UnpackedTickArray>, ProgramError> {
+  let loaded = loader.load()?;
 
-fn try_deserialize_tick_array(owner: &Pubkey, buf: &mut &[u8]) -> std::result::Result<Box<UnpackedTickArray>, Error> {
-  if !owner.eq(&crate::ID) {
-    return Err(UnpackError::OwnerConstraint.into());
-  }
+  let boxed = Box::new(UnpackedTickArray {
+    start_tick_index: loaded.start_tick_index,
+    ticks: loaded.ticks.iter().map(|t| UnpackedTick {
+      initialized: t.initialized,
+      liquidity_net: t.liquidity_net,
+      liquidity_gross: t.liquidity_gross,
+      fee_growth_outside_a: t.fee_growth_outside_a,
+      fee_growth_outside_b: t.fee_growth_outside_b,
+      reward_growths_outside: t.reward_growths_outside,
+    }).collect::<Vec<_>>().try_into().unwrap(),
+    whirlpool: loaded.whirlpool,
+  });
 
-  let mut cursor = 0;
-
-  let discriminator: u64 = bincode::deserialize(&buf[cursor..cursor+8]).unwrap();
-  cursor += 8;
-
-  // DISCRIMINATOR: 45 61 bd be 6e 07 42 bb
-  if discriminator != 0xbb42076ebebd6145 /* LE u64 */ {
-    return Err(UnpackError::AccountDiscriminatorMismatch.into());
-  }
-
-  let mut tickarray = UnpackedTickArray::default_boxed();
-
-  tickarray.start_tick_index = bincode::deserialize(&buf[cursor..cursor+4]).unwrap();
-  cursor += 4;
-
-  for i in 0..TICK_ARRAY_SIZE_USIZE {
-    tickarray.ticks[i] = UnpackedTick::deserialize(&mut &buf[cursor..cursor+Tick::LEN]).unwrap();
-    cursor += Tick::LEN;
-  }
-
-  tickarray.whirlpool = Pubkey::deserialize(&mut &buf[cursor..cursor+32]).unwrap();
-
-  Ok(tickarray)
-}
-
-#[error]
-pub enum UnpackError {
-  #[msg("Owner constraint violated")]
-  OwnerConstraint,
-  #[msg("Account discriminator mismatch")]
-  AccountDiscriminatorMismatch,
+  Ok(boxed)
 }
